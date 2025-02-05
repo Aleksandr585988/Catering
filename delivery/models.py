f4ecd86ecc4e73fe1516db9ef3c4d349b6bb8e14
Aleksandr Koltsow <koltsovoleksandr10081989@gmail.com>
2025-02-05 21:32:58 +0300

    Removed unnecessary files and updated project structure

---
+++ delivery/models.py
@@ -0,0 +1,12 @@
+
+from django.db import models
+from orders.models import Order
+
+
+class DeliveryOrder(models.Model):
+    provider = models.CharField(max_length=100)
+    status = models.CharField(max_length=50)
+    addresses = models.TextField()
+    external_order_id = models.CharField(max_length=255)
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
