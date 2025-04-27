c1c3b1a0a01fc0596f46e9c2a4b803b09b487504
Aleksandr Koltsow <koltsovoleksandr10081989@gmail.com>
2025-04-27 23:55:15 +0300

    commit

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from food.api import bueno_webhook
 from food.api import router as food_router
 from users.api import router as users_router
+from django.conf.urls.static import static
+from django.conf import settings
 
 # def user_create_api(request):
 #     raise NotImplementedError
@@ -18,3 +20,6 @@ urlpatterns = (
     + users_router.urls
     + food_router.urls
 )
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
